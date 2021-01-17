3409bbe5c99ab8236c536c86b2e7c8f7134cad47
Jürgen Hahn <mail.jhahn@googlemail.com>
2021-01-17 10:16:28 +0100

    fix signal length

--- app/src/main/java/com/freeflowgfx/convolution/Kotlin.kt
+++ app/src/main/java/com/freeflowgfx/convolution/Kotlin.kt
@@ -5,7 +5,7 @@ class Kotlin {
     fun convolution(signal: List<Float>, filter: List<Float>): List<Float> {
         val filterReversed = filter.reversed()
 
-        return (List(filter.size / 2) { 0f } + signal + List(filter.size / 2) { 0f }).windowed(
+        return (List(filter.size / 2) { 0f } + signal + List(filter.size / 2 - 1) { 0f }).windowed(
             filter.size
         ) {
             it.zip(filterReversed).map {
@@ -19,7 +19,6 @@ class Kotlin {
         val result = FloatArray(signal.size)
 
         for (i in signal.indices) {
-            result[i] = 0f
             for (j in filter.indices) {
                 val signalIndex: Int = i + j - filter.size / 2
                 if (signalIndex >= 0 && signalIndex < signal.size) {
